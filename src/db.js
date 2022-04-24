ef5ec656cc9a10f9c8c31c1f0d531ddbfdab7a06
thiagok2 <thiagok2@gmail.com>
2022-04-24 17:12:51 -0300

    commit app v1

---
+++ src/db.js
@@ -0,0 +1,7 @@
+const { Sequelize } = require('sequelize');
+
+const dbSequelize = new Sequelize('alunos', 'root', 'mysql', {
+    dialect:'mysql', host:'localhost'
+});
+
+module.exports = dbSequelize
